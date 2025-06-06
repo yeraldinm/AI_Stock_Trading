7cabc1c6d2d7bc674933de49b8bdcebf9acc63a6
yeraldinm <yeraldinmueses@gmail.com>
2025-06-06 12:33:40 -0400

    Fix day count increment

--- AI_Stock_Trading.py
+++ AI_Stock_Trading.py
@@ -158,6 +158,7 @@ class PortfolioManagementSystem(TradingSystem):
                 data=[[
                     data_req['IBM']['close'][0]]], columns='Close'.split()
             )
+            day_count += 1
             if(day_count == 7):
                 day_count = 0
                 last_weeks_close = this_weeks_close
