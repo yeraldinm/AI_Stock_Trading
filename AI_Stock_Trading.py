c21ea14c7e800ca6a7edf6da1e11b8cb4c545667
yeraldinm <yeraldinmueses@gmail.com>
2025-06-06 12:36:16 -0400

    Fix typo in comment

--- AI_Stock_Trading.py
+++ AI_Stock_Trading.py
@@ -55,7 +55,7 @@ class PMModelDevelopment:
         X = data['Delta Close']
         y = data.drop(['Delta Close'], axis=1)
 
-        # Train test spit
+        # Train test split
         X_train, X_test, y_train, y_test = train_test_split(X, y)
 
         # Create the sequential
